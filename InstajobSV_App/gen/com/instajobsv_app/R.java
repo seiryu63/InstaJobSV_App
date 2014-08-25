0eca0be7b6f8c5438d031c49a39ade2a76460442
seiryu63 <veta6363gmail.com>
2014-08-24 20:39:18 -0600

    Correccion de encabezado, pestañas

--- InstajobSV_App/gen/com/instajobsv_app/R.java
+++ InstajobSV_App/gen/com/instajobsv_app/R.java
@@ -22,10 +22,34 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_accept=0x7f020000;
+        public static final int ic_action_cancel=0x7f020001;
+        public static final int ic_action_important=0x7f020002;
+        public static final int ic_action_labels=0x7f020003;
+        public static final int ic_action_logo=0x7f020004;
+        public static final int ic_action_new_account=0x7f020005;
+        public static final int ic_action_overflow=0x7f020006;
+        public static final int ic_action_person=0x7f020007;
+        public static final int ic_action_search=0x7f020008;
+        public static final int ic_action_settings=0x7f020009;
+        public static final int ic_action_share=0x7f02000a;
+        public static final int ic_launcher=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int LinearLayout2=0x7f080001;
+        public static final int action_settings=0x7f08000d;
+        public static final int imageButton1=0x7f080002;
+        public static final int imageButton2=0x7f080004;
+        public static final int imageButton3=0x7f080003;
+        public static final int tab1=0x7f080005;
+        public static final int tab2=0x7f080007;
+        public static final int tab3=0x7f080009;
+        public static final int tab4=0x7f08000b;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f080008;
+        public static final int textView3=0x7f08000a;
+        public static final int textView4=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,6 +71,10 @@ public final class R {
         public static final int insertar_Registro=0x7f050005;
         public static final int nombre_proyecto=0x7f050003;
         public static final int password_user=0x7f05000b;
+        public static final int tab1=0x7f05000d;
+        public static final int tab2=0x7f05000e;
+        public static final int tab3=0x7f05000f;
+        public static final int tab4=0x7f050010;
         public static final int title_activity_usuario=0x7f05000c;
         public static final int usuario_menu=0x7f050004;
     }
--- InstajobSV_App/src/com/instajobsv_app/MainActivity.java
+++ InstajobSV_App/src/com/instajobsv_app/MainActivity.java
@@ -1,43 +1,65 @@
 package com.instajobsv_app;
 
+import android.R.color;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-//t
+import android.widget.TabHost;
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TabHost.OnTabChangeListener;
 
-public class MainActivity extends ListActivity {
+
+public class MainActivity extends Activity {
 	
-	String[] menu={"Tabla usuario","Llenar Base de Datos"};
-	String[] activities={"UsuarioActivity"};  
-
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menu));
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        
+        
+        Resources res = getResources();
+        TabHost tabs=(TabHost)findViewById(android.R.id.tabhost);
+       tabs.setup();
+       tabs.setBackgroundColor(Color.LTGRAY);
+   TabHost.TabSpec spec=tabs.newTabSpec("mitab1");
+   spec.setContent(R.id.tab1);
+   spec.setIndicator(null,res.getDrawable(R.drawable.ic_action_search));
+   tabs.addTab(spec);
+       
+   spec=tabs.newTabSpec("mitab2");
+   spec.setContent(R.id.tab2);
+   spec.setIndicator(null,res.getDrawable(R.drawable.ic_action_important));
+   tabs.addTab(spec);
+   
+   spec=tabs.newTabSpec("mitab3");
+   spec.setContent(R.id.tab3);
+   spec.setIndicator(null,res.getDrawable(R.drawable.ic_action_labels));
+   tabs.addTab(spec);
+       
+   spec=tabs.newTabSpec("mitab4");
+   spec.setContent(R.id.tab4);
+   spec.setIndicator(null,res.getDrawable(R.drawable.ic_action_person));
+   tabs.addTab(spec);
+   
+   tabs.setCurrentTab(0);
+       
+       
+   tabs.setOnTabChangedListener(new OnTabChangeListener() {
+              public void onTabChanged(String tabId) {
+              Log.i("AndroidTabsDemo", "Pulsada pestaña: " + tabId);
+              }
+            });
+        } 
     }
 
-    @Override
-    protected void onListItemClick(ListView l,View v,int position,long id){
-     super.onListItemClick(l, v, position, id);
-     
-     if(position!=1){
-      
-      String nombreValue=activities[position];
-     
-      try{
-       Class<?>
-       clase=Class.forName("com.instajobsv_app."+nombreValue);
-       Intent inte = new Intent(this,clase);
-       this.startActivity(inte);
-      }catch(ClassNotFoundException e){
-       e.printStackTrace();
-      }
-     
-     }else{
-     }
-   }
-} 
 
